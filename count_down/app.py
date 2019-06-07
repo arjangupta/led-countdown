39c4bca4ce05cb2773c364e19f5e6ac1d4e961af
Arjan Gupta <arjangupta95@gmail.com>
2019-06-07 03:26:42 -0500

    Attempt to import led functions as modules

--- count_down/app.py
+++ count_down/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template
-from flask_socketio import FlaskIO
+from flask_socketio import SocketIO, emit
+from importlib.machinery import SourceFileLoader
 
+led = SourceFileLoader("led", "../led_control/LED.py").load_module() 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
