e74e33642c37dc0d988030ecf662687b124d8847
Arjan Gupta <arjangupta95@gmail.com>
2019-06-07 04:00:29 -0500

    Convert main app into websocket app

--- count_down/app.py
+++ count_down/app.py
@@ -3,7 +3,10 @@ from flask_socketio import SocketIO, emit
 from importlib.machinery import SourceFileLoader
 
 led = SourceFileLoader("led", "../led_control/LED.py").load_module() 
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 @app.route('/', methods=['GET'])
 def index():
@@ -12,4 +15,4 @@ def index():
 if __name__ == '__main__':
     #led.red_led_off()
     #led.green_led_off()
-    app.run(debug=True, host='0.0.0.0')
+    socketio.run(app, debug=True, host='0.0.0.0')
